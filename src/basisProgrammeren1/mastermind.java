1b1aa097e5a224f89773acb60aeebcad2613ec21
pjotrevers <1224852@student.roc-nijmegen.nl>
2026-09-16 11:04:26 +0200

    initial commit

---
+++ src/basisProgrammeren1/mastermind.java
@@ -0,0 +1,138 @@
+package basisProgrammeren1;
+
+public class mastermind {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+//	Pionnen:
+		String blauwePion = "blauw";
+		String groenePion = "groen";
+		String gelePion = "geel";
+		String rodePion = "rood";
+		String paarsePion = "paars";
+		String oranjePion = "oranje";
+
+//		Pinnen:
+		String zwartePin = "leeg";
+		String wittePin = "leeg";
+		String geenPin = "leeg";
+
+//		Rijvakken:
+		String rij1Vak1 = "";
+		String rij1Vak2 = "";
+		String rij1Vak3 = "";
+		String rij1Vak4 = "";
+
+		String rij2Vak1 = "";
+		String rij2Vak2 = "";
+		String rij2Vak3 = "";
+		String rij2Vak4 = "";
+
+		String rij3Vak1 = "";
+		String rij3Vak2 = "";
+		String rij3Vak3 = "";
+		String rij3Vak4 = "";
+		String rij3Vak4 = "";
+
+		String rij4Vak4 = "";
+		String rij4Vak4 = "";
+		String rij4Vak4 = "";
+		String rij4Vak4 = "";
+
+		String rij5Vak1 = "";
+		String rij5Vak2 = "";
+		String rij5Vak3 = "";
+		String rij5Vak4 = "";
+
+
+		String rij6Vak1 = "";
+		String rij6Vak2 = "";
+		String rij6Vak3 = "";
+		String rij6Vak4 = "";
+
+		String rij7Vak1 = "";
+		String rij7Vak2 = "";
+		String rij7Vak3 = "";
+		String rij7Vak4 = "";
+
+		String rij8Vak1 = "";
+		String rij8Vak2 = "";
+		String rij8Vak3 = "";
+		String rij8Vak4 = "";
+
+		String rij9Vak1 = "";
+		String rij9Vak2 = "";
+		String rij9Vak3 = "";
+		String rij9Vak4 = "";
+
+		String rij10Vak1 = "";
+		String rij10Vak2 = "";
+		String rij10Vak3 = "";
+		String rij10Vak4 = "";
+
+//		Verborgenrijvakken:
+		String verborgenRijVak1 = "blauw";
+		String verborgenRijVak2 = "groen";
+		String verborgenRijVak3 = "paars";
+		String verborgenRijVak4 = "oranje";
+
+//		Spelers:
+		String speler1 = "";
+		String speler2 = "";
+
+//		Checkvakken:
+		String rij1CheckVak1 = "";
+		String rij1CheckVak2 = "";
+		String rij1CheckVak3 = "";
+		String rij1CheckVak4 = "";
+
+		String rij2CheckVak1 = "";
+		String rij2CheckVak2 = "";
+		String rij2CheckVak3 = "";
+		String rij2CheckVak4 = "";
+
+		String rij3CheckVak1 = "";
+		String rij3CheckVak2 = "";
+		String rij3CheckVak3 = "";
+		String rij3CheckVak4 = "";
+
+		String rij4CheckVak1 = "";
+		String rij4CheckVak2 = "";
+		String rij4CheckVak3 = "";
+		String rij4CheckVak4 = "";
+
+		String rij5CheckVak1 = "";
+		String rij5CheckVak2 = "";
+		String rij5CheckVak3 = "";
+		String rij5CheckVak4 = "";
+
+		String rij6CheckVak1 = "";
+		String rij6CheckVak2 = "";
+		String rij6CheckVak3 = "";
+		String rij6CheckVak4 = "";
+
+		String rij7CheckVak1 = "";
+		String rij7CheckVak2 = "";
+		String rij7CheckVak3 = "";
+		String rij7CheckVak4 = "";
+
+		String rij8CheckVak1 = "";
+		String rij8CheckVak2 = "";
+		String rij8CheckVak3 = "";
+		String rij8CheckVak4 = "";
+
+
+
+		String rij9CheckVak1 = "";
+		String rij9CheckVak2 = "";
+		String rij9CheckVak3 = "";
+		String rij9CheckVak4 = "";
+
+		String rij10CheckVak1 = "";
+		String rij10CheckVak2 = "";
+		String rij10CheckVak3 = "";
+		String rij10CheckVak4 = "";
+
+}
